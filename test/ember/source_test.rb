f350fffe23228f0f9eeabc5a7a2175f62bc81cde
OsamaSayegh <asooomaasoooma90@gmail.com>
2020-11-28 12:55:37 +0300

    DEV: Switch from Travis to Actions

--- test/ember/source_test.rb
+++ test/ember/source_test.rb
@@ -2,6 +2,6 @@ require "test_helper"
 
 class Ember::SourceTest < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::Ember::Source::VERSION
+    refute_nil ::Ember::VERSION
   end
 end
